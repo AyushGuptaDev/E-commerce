6378f6cced10d04018e383e3aa717bf93da45405
AyushGuptaDev <ayush11gupta2003@gmail.com>
2025-08-31 03:05:01 +0530

    seach bar and start on cart drawer

--- frontend/src/components/Comman/Navbar.jsx
+++ frontend/src/components/Comman/Navbar.jsx
@@ -5,66 +5,73 @@ import {
   HiBars3BottomRight,
 } from "react-icons/hi2";
 import SearchBar from "./SearchBar";
+import CartDrawer from "../Layout/CartDrawer";
 
 const Navbar = () => {
   return (
-    <nav className="container mx-auto flex items-center justify-between px-6 py-4">
-      {/* left logo */}
-      <div>
-        <Link to="/" className="text-2xl font-medium">
-          Rabbit
-        </Link>
-      </div>
-      {/* centre */}
-      <div className="hidden md:flex space-x-6">
-        <Link
-          to="#"
-          className="text-gray-700 hover:text-black  text-sm font-medium uppercase"
-        >
-          Men
-        </Link>
+    <>
+      <nav className="container mx-auto flex items-center justify-between px-6 py-4">
+        {/* left logo */}
+        <div>
+          <Link to="/" className="text-2xl font-medium">
+            Rabbit
+          </Link>
+        </div>
+        {/* centre */}
+        <div className="hidden md:flex space-x-6">
+          <Link
+            to="#"
+            className="text-gray-700 hover:text-black  text-sm font-medium uppercase"
+          >
+            Men
+          </Link>
 
-        <Link
-          to="#"
-          className="text-gray-700 hover:text-black  text-sm font-medium uppercase"
-        >
-          WoMen
-        </Link>
+          <Link
+            to="#"
+            className="text-gray-700 hover:text-black  text-sm font-medium uppercase"
+          >
+            WoMen
+          </Link>
 
-        <Link
-          to="#"
-          className="text-gray-700 hover:text-black  text-sm font-medium uppercase"
-        >
-          Top WEAR
-        </Link>
+          <Link
+            to="#"
+            className="text-gray-700 hover:text-black  text-sm font-medium uppercase"
+          >
+            Top WEAR
+          </Link>
 
-        <Link
-          to="#"
-          className="text-gray-700 hover:text-black  text-sm font-medium uppercase"
-        >
-          Bottom Wear
-        </Link>
-      </div>
-      {/* right */}
-      <div className="flex items-center space-x-4">
-        <Link to="/" className="hover:text-black">
-          <HiOutlineUser className="h-6 w-6 text-gray-700" />
-        </Link>
+          <Link
+            to="#"
+            className="text-gray-700 hover:text-black  text-sm font-medium uppercase"
+          >
+            Bottom Wear
+          </Link>
+        </div>
+        {/* right */}
+        <div className="flex items-center space-x-4">
+          <Link to="/" className="hover:text-black">
+            <HiOutlineUser className="h-6 w-6 text-gray-700" />
+          </Link>
 
-        <button className="relative hover:text-black">
-          <HiOutlineShoppingBag className="w-6 h-6 text-gray-700" />
-          <span className="absolute top-1 bg-rabbit-red text-white text-xs rounded-full py-0.2 px-2">
-            4
-          </span>
-        </button>
+          <button className="relative hover:text-black">
+            <HiOutlineShoppingBag className="w-6 h-6 text-gray-700" />
+            <span className="absolute top-1 bg-rabbit-red text-white text-xs rounded-full py-0.2 px-2">
+              4
+            </span>
+          </button>
 
-        <SearchBar />  
+          <div className="overflow-hidden">
+            <SearchBar />
+          </div>
 
-        <button className="md:hidden">
-          <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
-        </button>
-      </div>
-    </nav>
+          <button className="md:hidden">
+            <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
+          </button>
+        </div>
+      </nav>
+
+      <CartDrawer />
+    </>
   );
 };
 
--- frontend/src/components/Comman/SearchBar.jsx
+++ frontend/src/components/Comman/SearchBar.jsx
@@ -1,26 +1,55 @@
 import { useState } from "react";
-import { HiMagnifyingGlass } from "react-icons/hi2";
+import { HiMagnifyingGlass, HiMiniXMark } from "react-icons/hi2";
 
 const SearchBar = () => {
-  const [SearchTern, setSearchTerm] = useState("");
+  const [SearchTerm, setSearchTerm] = useState("");
   const [isOpen, setIsOpen] = useState(false);
 
   const HandleSeachToggle = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("search term : ", SearchTerm);
+    setIsOpen(false);
+  };
+
   return (
-    <div>
+    <div
+      className={`flex items-center justify-center w-full transition-all duration-300 ${
+        isOpen ? "absolute top-0 left-0 w-full bg-white h-24 z-50" : "w-auto"
+      }`}
+    >
       {isOpen ? (
-        <form className="relative flex justify-center items-center w-full">
+        <form
+          className="relative flex justify-center items-center w-full"
+          onSubmit={handleSubmit}
+        >
           <div className=" relative w-1/2">
             <input
               type="text"
               placeholder="search"
-              value={SearchTern}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              value={SearchTerm}
               className="bg-gray-100 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none w-full placeholder:text-gray-700"
             />
+
+            <button
+              type="submit"
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-gray-800"
+            >
+              <HiMagnifyingGlass className="h-6 w-6" />
+            </button>
           </div>
+
+          <button
+            type="button"
+            className="absolute right-10 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-gray-800"
+            onClick={HandleSeachToggle}
+          >
+            <HiMiniXMark className="x-6 w-6" />
+          </button>
         </form>
       ) : (
         <button onClick={HandleSeachToggle}>
